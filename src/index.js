bfcb3bf3d6eb5d6f75ac65e862610e48160d319c
Isigba Ambrose <ambrisexyz@gmail.com>
2022-07-28 17:11:01 +0200

    fetch data from the server using API fecth function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './index.css';
+ import './index.css';
+import { fetchScore , addToScore } from './modules/app.js';
+const formInput = document.querySelector('#form-input');
+const user_input = document.querySelector('#user_input');
+const score_input = document.querySelector('#score_input');
+const refresh_btn = document.querySelector('#refresh-btn');
+
+formInput.addEventListener('submit', (el)=>{
+  el.preventDefault();
+  addToScore(user_input.value, score_input.value)
+  formInput.reset();
+});
+ refresh_btn.addEventListener('click', ()=>{
+fetchScore()
+ })
---
+++ src/modules/app.js
@@ -0,0 +1,48 @@
+const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+ const gameId = 'YezulV9ho9Ix3Z5X3qdY'; 
+const addUrlScores = `${API_URL + gameId}/scores`;
+const createGame = async () => {
+  await fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify({
+     name: 'My cool new game',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => (json));
+};
+  createGame()
+
+function scoreTable(scores) {
+  let output = '';
+  scores.forEach((game) => {
+    output += `<li>${game.user}: ${game.score}</li>`;
+  });
+  document.querySelector('.score-list').innerHTML = output;
+}
+// to fetch added score from the API
+const fetchScore = async () => {
+  await fetch(addUrlScores)
+    .then((response) => response.json())
+    .then((json) => scoreTable(json.result));
+};
+
+const addToScore = async (user, score) => {
+  await fetch(addUrlScores, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+      },
+   
+    body: JSON.stringify({
+      user, score,
+    }),
+     }).then((res) => res.json()).then((json) =>{
+      fetchScore(json)
+    }).catch((error) => (error))
+};
+export { fetchScore, addToScore }
